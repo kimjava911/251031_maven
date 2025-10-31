07915c19d953a0237a6b39bfa35e5dd047f9a83a
kimjava911 <aibackend4th@gmail.com>
2025-10-31 10:14:28 +0900

    dotenv -> secret 관리에 좋음

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,15 @@
+// 회사의 도메인(url)을 의미
+// 구분이 안된다 -> 다른 모듈(라이브러리)와 구분을 하기 위해 본인 회사, 소속, ...
+// kr.co.programmers ...
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Dotenv dotenv = Dotenv.load(); // .env를 읽어들여와서 get으로 쓸 수 있게 만들어줌
+        System.out.println(dotenv.get("HELLO"));
     }
 }
